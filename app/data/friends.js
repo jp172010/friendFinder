05ed88670962733acef726f45ccfb0b819fef049
JonP <jp17_2010@yahoo.com>
2019-06-07 20:07:34 -0500

    make it better

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,72 @@
-var friendsArray = [];
+var friendsArray = [
+    {
+        name: "Jon",
+        Photo: "",
+        scores: [
+            "3",
+            "4",
+            "2",
+            "4",
+            "5",
+            "3",
+            "4",
+            "2",
+            "4",
+            "5"
+        ],
+        sum: 36
+    },
+    {
+        name: "Shannon",
+        Photo: "",
+        scores: [
+            "2",
+            "1",
+            "4",
+            "5",
+            "5",
+            "5",
+            "3",
+            "3",
+            "3",
+            "3"
+        ],
+        sum: 34
+    },
+    {
+        name: "Jessica",
+        Photo: "",
+        scores: [
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4"
+        ],
+        sum: 40
+    },
+    {
+        name: "Amanda",
+        Photo: "",
+        scores: [
+            "5",
+            "5",
+            "3",
+            "2",
+            "1",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"
+        ],
+        sum: 31
+    }
+];
 
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,6 +7,22 @@ module.exports = function(app){
 
     app.post("/api/friends", function(req, res){
         friendsData.push(req.body);
+
+        let newest = friendsData.length - 1;
+        function getSum(total, num){
+            return total + num;
+        };
+        let int = friendsData[newest].scores.map(parseInt)
+        console.log(int)
+        //let sum = int.reduce(getSum);
+        //friendsData[newest].sum = sum;
+
+        //function findFriend(arr){
+          //  for(var i = 0; i < friendsData.length -1; i++)
+           // friendsData[newest].sum - arr[i].sum
+       // };
+        
+        
     });
 
     app.post("/api/clear", function(req, res) {
